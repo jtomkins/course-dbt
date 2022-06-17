b3b750848ef9ed55e2617abd3a18da5c19f0eb4b
jtomkins <jtomkins@ucsc.edu>
2022-06-17 20:16:32 +0000

    addresses to fct user orders

--- greenery/models/marts/Marketing/fct_user_order.sql
+++ greenery/models/marts/Marketing/fct_user_order.sql
@@ -9,6 +9,10 @@ SELECT
     ,stg_greenery__users.phone_number
     ,stg_greenery__users.created_at_utc
     ,stg_greenery__users.updated_at_utc
+    ,stg_greenery__addresses.street_address
+    ,stg_greenery__addresses.state
+    ,stg_greenery__addresses.country
+    ,stg_greenery__addresses.zip_code
     ,int_orders_promo_id_agg.order_guid
     ,int_orders_promo_id_agg.instruction_set
     ,int_orders_promo_id_agg.digitized
@@ -18,10 +22,11 @@ SELECT
     ,int_orders_promo_id_agg.task_force
     ,int_orders_delivery_basic.number_of_days_to_deliver
     ,int_orders_delivery_basic.diff_in_estimated_delivery
-    
-    
+   
 from {{  ref('int_orders_promo_id_agg')  }}
 left join {{  ref('int_orders_delivery_basic')  }}
     on int_orders_promo_id_agg.order_guid = int_orders_delivery_basic.order_guid
 left join {{  ref('stg_greenery__users')  }}
     on  int_orders_promo_id_agg.user_guid = stg_greenery__users.user_guid
+LEFT JOIN {{ ref('stg_greenery__addresses') }}
+  ON stg_greenery__users.address_guid =stg_greenery__addresses.address_guid
