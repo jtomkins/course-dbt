557c297f3ceae969faaec51f4d90ab2e4f762b5e
jtomkins <jtomkins@ucsc.edu>
2022-06-18 22:06:44 +0000

    total products sold

--- greenery/models/marts/Core/dim_products.sql
+++ greenery/models/marts/Core/dim_products.sql
@@ -9,7 +9,7 @@ SELECT
   ,stg_greenery__products.product_name
   ,stg_greenery__products.price
   ,stg_greenery__products.inventory
-  ,int_products_sold.number_sold
+  ,int_products_sold.total_number_sold
   FROM {{ ref('stg_greenery__products') }} 
     Left join {{ ref('int_products_sold')}}
     on stg_greenery__products.product_guid = int_products_sold.product_guid 
--- greenery/models/marts/Core/intermediate/int_total_products_sold.sql
+++ greenery/models/marts/Core/intermediate/int_total_products_sold.sql
@@ -1,7 +1,7 @@
 {{ config (materialized='table')}}
 
 with number_of_products_sold as (
-    select oi.product_guid, sum(oi.quantity) number_sold,  p.product_name
+    select oi.product_guid, sum(oi.quantity) total_number_sold,  p.product_name
     from dbt_jen_w.stg_greenery__order_items oi
     left outer join dbt_jen_w.stg_greenery__products p
         on oi.product_guid = p.product_guid
