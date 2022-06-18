70509a04cbdc369cc7247aa239bc429f6f98804e
jtomkins <jtomkins@ucsc.edu>
2022-06-18 21:43:02 +0000

    checking in products

---
+++ greenery/models/marts/Core/dim_products.sql
@@ -0,0 +1,16 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+SELECT
+  stg_greenery__products.product_guid
+  ,stg_greenery__products.product_name
+  ,stg_greenery__products.price
+  ,stg_greenery__products.inventory
+  ,int_products_sold.number_sold
+  FROM {{ ref('stg_greenery__products') }} 
+    Left join {{ ref('int_products_sold')}}
+    on stg_greenery__products.product_guid = int_products_sold.product_guid 
+
