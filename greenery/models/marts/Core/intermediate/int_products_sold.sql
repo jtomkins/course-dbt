e194346ba0e816338e8607451639be736923069f
jtomkins <jtomkins@ucsc.edu>
2022-06-18 21:58:05 +0000

    using quantity field instead

--- greenery/models/marts/Core/intermediate/int_products_sold.sql
+++ greenery/models/marts/Core/intermediate/int_products_sold.sql
@@ -1,8 +1,7 @@
 {{ config (materialized='table')}}
 
---time order placed to be delivered
 with number_of_products_sold as (
-    select oi.product_guid, count(oi.product_guid) number_sold,  p.product_name
+    select oi.product_guid, sum(oi.quantity) number_sold,  p.product_name
     from dbt_jen_w.stg_greenery__order_items oi
     left outer join dbt_jen_w.stg_greenery__products p
         on oi.product_guid = p.product_guid
