64ab0deed967d070c0278f58387caa7889dd3709
jtomkins <jtomkins@ucsc.edu>
2022-06-18 22:40:48 +0000

    total products sold

--- greenery/models/marts/Core/intermediate/int_total_products_sold.sql
+++ greenery/models/marts/Core/intermediate/int_total_products_sold.sql
@@ -2,8 +2,8 @@
 
 with total_number_of_products_sold as (
     select oi.product_guid, sum(oi.quantity) total_number_sold,  p.product_name
-    from dbt_jen_w.stg_greenery__order_items oi
-    left outer join dbt_jen_w.stg_greenery__products p
+from {{ ref ('stg_greenery__order_items') }}  oi
+left outer join {{ ref ('stg_greenery__products') }}  p
         on oi.product_guid = p.product_guid
     group by oi.product_guid, p.product_name
 )
--- greenery/models/marts/Marketing/intermediate/int_products_sold_per_order.sql
+++ greenery/models/marts/Marketing/intermediate/int_products_sold_per_order.sql
@@ -3,8 +3,8 @@
 with number_of_products_sold_per_order as (
 
             select  order_guid, p.product_guid, sum(quantity) as number_sold_per_order, product_name
-            from dbt_jen_w.stg_greenery__order_items oi
-            left outer join dbt_jen_w.stg_greenery__products p
+            from {{ ref ('stg_greenery__order_items') }}  oi
+            left outer join {{ ref ('stg_greenery__products') }}  p
                     on oi.product_guid = p.product_guid
             group by order_guid, p.product_guid, quantity, product_name
             order by order_guid
