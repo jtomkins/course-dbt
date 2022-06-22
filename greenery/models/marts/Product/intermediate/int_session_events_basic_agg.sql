43b1dec1160db7a14a8212f75f26b34e0c076e8c
jtomkins <jtomkins@ucsc.edu>
2022-06-22 23:03:11 +0000

    include scratch and fix typo

--- greenery/models/marts/Product/intermediate/int_session_events_basic_agg.sql
+++ greenery/models/marts/Product/intermediate/int_session_events_basic_agg.sql
@@ -6,7 +6,7 @@ SELECT
     ,user_guid
     ,sum(case when event_type = 'page_view' then 1 else 0 end) as page_view
     ,sum(case when event_type = 'checkout' then 1 else 0 end) as checkout
-    ,sum(case when event_type = 'add_to_card' then 1 else 0 end) as add_to_cart
+    ,sum(case when event_type = 'add_to_cart' then 1 else 0 end) as add_to_cart
 	,sum(case when event_type = 'package_shipped' then 1 else 0 end) as package_shipped
 from {{ ref ('stg_greenery__events') }}
 group by 1,2,3
