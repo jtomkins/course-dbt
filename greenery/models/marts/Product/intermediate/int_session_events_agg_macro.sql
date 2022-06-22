89e512302fb80e0186c615903fd1052cac6490ac
jtomkins <jtomkins@ucsc.edu>
2022-06-22 23:48:06 +0000

    updated packages, scratch and test session events agg macro

---
+++ greenery/models/marts/Product/intermediate/int_session_events_agg_macro.sql
@@ -0,0 +1,18 @@
+{{ config(materialized = 'table')  }}
+
+{%
+    set event_types = dbt_utils.get_query_results_as_dict (
+            "select distinct quote_literal(event_type) as event_type, event_type as column_name from"
+            ~ ref('stg_greenery__events')
+    )
+%}
+
+SELECT
+    session_guid
+    ,created_at_utc
+    ,user_guid
+     {% for event_type in event_types['event_type'] %}
+        ,sum(case when event_type = {{event_type}} then 1 else 0 end) as {{ event_types['column_name'][loop.index0] }}
+    {% endfor %}
+from {{ ref('stg_greenery__events') }}
+group by 1,2,3
