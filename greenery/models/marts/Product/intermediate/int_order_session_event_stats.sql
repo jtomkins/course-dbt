2a8b6be984a6411858b6733017bdb61b18f74925
jtomkins <jtomkins@ucsc.edu>
2022-06-23 20:58:28 +0000

    checking in int order session event stats

---
+++ greenery/models/marts/Product/intermediate/int_order_session_event_stats.sql
@@ -0,0 +1,26 @@
+{{ config(materialized = 'table')  }}
+
+
+with session_by_product  as (
+    select product_guid as sbp_product_guid,
+            count(distinct session_guid) filter (where page_view > 0) as unique_session_viewed_each_product
+    from dbt_jen_w.int_session_events_agg_macro
+    where product_guid is not null
+    and page_view > 0
+    group by 1
+    order by 1
+), order_events_stats as (
+select 
+  o.product_guid as oes_product_guid,
+  o.product_name as oes_product_name,
+  count(distinct  session_guid ) filter (where checkout > 0) as unique_user_sessions_with_checkouts
+from {{ ref('int_session_events_agg_macro') }} m
+    inner join {{ ref('int_products_sold_per_order') }} o
+      on m.order_guid = o.order_guid
+and checkout > 0
+  group by 1,2
+)
+select sbp.*, oes.* 
+from session_by_product sbp
+    left join order_events_stats oes
+    on sbp.sbp_product_guid = oes.oes_product_guid
