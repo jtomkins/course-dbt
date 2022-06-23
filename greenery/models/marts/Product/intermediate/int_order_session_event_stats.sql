2a83eca5209374957dcd248eabe6126a7eea8002
jtomkins <jtomkins@ucsc.edu>
2022-06-23 21:35:07 +0000

    added add to cart to consider for product conversion rate

--- greenery/models/marts/Product/intermediate/int_order_session_event_stats.sql
+++ greenery/models/marts/Product/intermediate/int_order_session_event_stats.sql
@@ -3,10 +3,11 @@
 
 with session_by_product  as (
     select product_guid as sbp_product_guid,
-            count(distinct session_guid) filter (where page_view > 0) as unique_session_viewed_each_product
-    from dbt_jen_w.int_session_events_agg_macro
+            count(distinct session_guid) filter (where page_view > 0) as unique_session_viewed_each_product,
+            count(distinct session_guid) filter (where add_to_cart > 0) as unique_user_sessions_with_add_to_cart
+ from dbt_jen_w.int_session_events_agg_macro
     where product_guid is not null
-    and page_view > 0
+    --and page_view > 0
     group by 1
     order by 1
 ), order_events_stats as (
@@ -14,13 +15,16 @@ select
   o.product_guid as oes_product_guid,
   o.product_name as oes_product_name,
   count(distinct  session_guid ) filter (where checkout > 0) as unique_user_sessions_with_checkouts
-from {{ ref('int_session_events_agg_macro') }} m
+  from {{ ref('int_session_events_agg_macro') }} m
     inner join {{ ref('int_products_sold_per_order') }} o
       on m.order_guid = o.order_guid
 and checkout > 0
   group by 1,2
 )
-select sbp.*, oes.*, (unique_user_sessions_with_checkouts::float/ unique_session_viewed_each_product::float) as product_conversion_rate
+select sbp.*, 
+      oes.*, 
+      (unique_user_sessions_with_checkouts::float/ unique_session_viewed_each_product::float) as _checkout_product_conversion_rate,
+      (unique_user_sessions_with_add_to_cart::float/ unique_session_viewed_each_product::float) as _add_to_cart_product_conversion_rate
 from session_by_product sbp
     left join order_events_stats oes
     on sbp.sbp_product_guid = oes.oes_product_guid
