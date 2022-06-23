31188b9d4321a429d4ff2c0e47f43907b1d27e0a
jtomkins <jtomkins@ucsc.edu>
2022-06-23 17:08:38 +0000

    rename column

--- greenery/models/marts/Product/intermediate/int_user_session_event_stats.sql
+++ greenery/models/marts/Product/intermediate/int_user_session_event_stats.sql
@@ -9,7 +9,7 @@ select
   count(distinct order_guid ) filter (where checkout > 0)  as total_orders_with_checkouts,
   count(distinct session_guid ) filter (where checkout > 0) as user_sessions_with_checkouts,
 	count(distinct session_guid ) filter (where package_shipped > 0) as user_sessions_with_shipment,
-	count(distinct session_guid ) filter (where add_to_cart > 0) as user_sessions_with_checkout,
+	count(distinct session_guid ) filter (where add_to_cart > 0) as user_sessions_with_add_to_cart,
   count(distinct session_guid ) filter (where page_view > 0) as number_of_unique_session_page_views,
   sum(add_to_cart) as total_add_to_cart,
 	sum(page_view) as total_page_views,
